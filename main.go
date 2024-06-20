ccdc7b0ca4a96f512fe52c8ef5df5a151f4522fb
ssoda <demaciasoda@gmail.com>
2024-06-20 22:58:35 +0800

    feat: add fibercaptcha package

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/ssoda/fibercaptcha"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(fibercaptcha.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
